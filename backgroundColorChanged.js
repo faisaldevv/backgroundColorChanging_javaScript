14797681d27b578cfca6fbbd90f7ea18dc5ca1ff
Faisal Khan <faisaldev404@gmail.com>
2023-03-02 16:16:40 +0500

    update: fix some minor changes

--- backgroundColorChanged.js
+++ backgroundColorChanged.js
@@ -1,9 +1,9 @@
 
 const btn = document.getElementById("colorChange");
-
+let hashColor
 const changeBackground = () => {
     let val = "1234567890ABCDEF";
-    let hashColor = "#";
+    hashColor = "#";
     for (let i = 0; i < 6; i++) {
         hashColor = hashColor + val[Math.floor(Math.random() * 16)];
     }
@@ -12,5 +12,7 @@ const changeBackground = () => {
 btn.addEventListener('click', triggerBackground)
 function triggerBackground() {
     document.body.style.backgroundColor = changeBackground();
+    console.log(hashColor)
+    console.log(this)
 }
 
