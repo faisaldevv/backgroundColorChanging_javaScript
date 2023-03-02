8a0952e9c6ca5ef9ef9228e92cffeddafda2ea9f
Faisal Khan <faisaldev404@gmail.com>
2023-03-02 10:12:01 +0500

    buttonCss
    button Css

--- backgroundColorChanged.js
+++ backgroundColorChanged.js
@@ -1,19 +1,18 @@
 
 const btn = document.getElementById("colorChange");
 
-let combineColor;
 const changeBackground = () => {
     let val = "1234567890ABCDEF";
     let hashColor = "#";
     for (let i = 0; i < 6; i++) {
-        combineColor = hashColor + val[Math.floor(Math.random() * 16)];
+        hashColor = hashColor + val[Math.floor(Math.random() * 16)];
     }
-    return combineColor
-    console.log(combineColor);
+    return hashColor;
+    console.log(hashColor);
+
 }
 btn.addEventListener('click', triggerBackground)
 function triggerBackground() {
-    document.body.style.backgroundColor = combineColor
-
+    document.body.style.backgroundColor = changeBackground();
 }
 
