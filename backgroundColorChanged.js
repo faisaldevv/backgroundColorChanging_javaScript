8eccdcee2148e19f76e9edb729f847ae8e15215c
Faisal Khan <faisaldev404@gmail.com>
2023-03-02 10:12:12 +0500

    Revert "buttonCss"
    This reverts commit 8a0952e9c6ca5ef9ef9228e92cffeddafda2ea9f.

--- backgroundColorChanged.js
+++ backgroundColorChanged.js
@@ -1,18 +1,19 @@
 
 const btn = document.getElementById("colorChange");
 
+let combineColor;
 const changeBackground = () => {
     let val = "1234567890ABCDEF";
     let hashColor = "#";
     for (let i = 0; i < 6; i++) {
-        hashColor = hashColor + val[Math.floor(Math.random() * 16)];
+        combineColor = hashColor + val[Math.floor(Math.random() * 16)];
     }
-    return hashColor;
-    console.log(hashColor);
-
+    return combineColor
+    console.log(combineColor);
 }
 btn.addEventListener('click', triggerBackground)
 function triggerBackground() {
-    document.body.style.backgroundColor = changeBackground();
+    document.body.style.backgroundColor = combineColor
+
 }
 
